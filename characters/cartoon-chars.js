fd9f36e534cd1239f95a671727dbc6bbcfe63ca3
Jasurjon Abduvayitov <jasurjonai@gmail.com>
2026-06-25 11:03:01 +0500

    Update cartoon-chars.js

--- characters/cartoon-chars.js
+++ characters/cartoon-chars.js
@@ -11,35 +11,35 @@ export const CARTOON_CHARS = [
     id: 'baby-boss',
     name: 'Baby Boss',
     image: 'characters/baby-boss.svg',
-    from: 'The Boss Baby (2017)',
+    from: 'The Boss Baby',
     animation: 'sway 3s ease-in-out infinite'
   },
   {
     id: 'puss-in-boots',
     name: 'Puss in Boots',
     image: 'characters/puss-in-boots.svg',
-    from: 'Shrek / DreamWorks',
+    from: 'DreamWorks',
     animation: 'float 2.8s ease-in-out infinite'
   },
   {
     id: 'baby-boss-girl',
     name: 'Baby Boss Girl',
     image: 'characters/baby-boss-girl.svg',
-    from: 'Family Business (2021)',
+    from: 'Family Business',
     animation: 'sway 3.4s ease-in-out infinite'
   },
   {
     id: 'puss-in-boots-nailles',
     name: 'Puss in Boots Nailles',
     image: 'characters/puss-in-boots-nailles.svg',
-    from: 'Shrek / DreamWorks',
+    from: 'DreamWorks',
     animation: 'float 3.1s ease-in-out infinite'
   },
   {
     id: 'kitty-softpaws',
-    name: 'Kitty Softpaws',
+    name: 'Tai Lung ',
     image: 'characters/kitty-softpaws.png',
-    from: 'Puss in Boots (2011)',
+    from: 'Dragon Warrior',
     animation: null
   },
 ];
